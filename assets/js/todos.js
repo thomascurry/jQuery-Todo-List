7d4b5afb6a5109de0073b321c5421fe8026c46b9
Thomas Curry <thomasjcurry@hotmail.com>
2017-02-18 13:53:38 +0000

    Finished off basic functionality
    Finished off basic functionality ready for styling.

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,24 @@
 // strike through todos by clicking 
-$("li").click(function() {
+$("ul").on("click", "li", function() {
     $(this).toggleClass('completed ');
 });
 
 // click on 'x' to delete todo
-$("span").click(function(event) {
-	$(this).parent().fadeOut(function() {
+$("ul").on("click", "span", function(event) {
+	$(this).parent().fadeOut(500, function() {
 		$(this).remove();
-	}
+	});
 	event.stopPropagation();
+});
+
+// get text from input
+$("input[type='text']").keypress(function(event) {
+	if (event.which === 13){
+		// store input text
+		var todoText = $(this).val();
+		// empty the input box
+		$(this).val("");
+		// add new todo
+		$("ul").append(`<li><span>X </span>${todoText}</li>`)
+	}
 });
